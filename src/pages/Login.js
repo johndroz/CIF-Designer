3af32ea66ed0c4dc0b71de022830655857aaa646
johndroz <66795002+johndroz@users.noreply.github.com>
2023-09-24 10:36:00 -0400

    login Form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,28 +3,32 @@ import React from 'react';
 class Login extends React.Component{
       login = (e)=>{
         e.preventDefault();
+        var formData = new FormData();
+        formData.append('username', e.target.username.value);
+        formData.append('password', e.target.password.value);
         var message = document.getElementById('message');
         message.innerHTML = '';
-        var url = '/users?username=' + e.target.username.value + '&password=' + e.target.password.value;
-        fetch(url)
-        .then((data)=> data.text())
-        .then((text)=> {
-            message.innerHTML = text;
-            console.log(text);
+        //var url = '/users?username=' + e.target.username.value + '&password=' + e.target.password.value;
+        var url = '/users';
+        fetch(url, {method: "POST", cache: "reload", body: new URLSearchParams(formData)})
+        .then((response)=>{
+          console.log(response);
+          return response.text();
+        })
+        .then(text=>{
+          message.innerHTML = text;
         })
         .catch((err)=> console.log(err));
-        
-
       }
 
       render(){
 
         return(
           <>
-            <form method="get" onSubmit={this.login}>
-                <input name="username" placeholder='Username'></input>
-                <input name="password" placeholder='password' type='password'></input>
-                <input type='submit' value="Submit"></input>
+            <form id="login" action="/users" method='post' onSubmit={this.login}>
+                <input id="username" name="username" placeholder='Username'></input>
+                <input id="password" name="password" placeholder='Password' type='password'></input>
+                <button type='submit'>Submit</button>
             </form>
 
             <p id="message"></p>
